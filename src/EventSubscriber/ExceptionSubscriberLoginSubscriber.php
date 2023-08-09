51d08ca9e5bd7ad1dc06fdb9e0ebafdd7494894d
Jesus Emmanuel Baca Hernández <jesus.e.b.h.one@gmail.com>
2023-08-09 13:16:34 -0600

    login avance: Event with dd

---
+++ src/EventSubscriber/ExceptionSubscriberLoginSubscriber.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\EventSubscriber;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\Security\Core\Event\AuthenticationSuccessEvent;
+
+class ExceptionSubscriberLoginSubscriber implements EventSubscriberInterface
+{
+    public function onSecurityAuthenticationSuccess(AuthenticationSuccessEvent $event): void
+    {
+        // ...
+
+        dd('here');
+    }
+
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            'security.authentication.success' => 'onSecurityAuthenticationSuccess',
+        ];
+    }
+}

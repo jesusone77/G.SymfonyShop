66de0383f145b2ce62e690148561bd8183b56510
Jesus Emmanuel Baca Hernández <jesus.e.b.h.one@gmail.com>
2023-08-09 14:44:38 -0600

    login bien direccionado con yaml

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -12,6 +12,7 @@ class LoginController extends AbstractController
     #[Route('/login', name: 'app_login')]
     public function index(AuthenticationUtils $authenticationUtils): Response
     {
+        // dd('parece que si regresa');
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
         // last username entered by the user

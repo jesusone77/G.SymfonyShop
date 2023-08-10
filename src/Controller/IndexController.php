60b86235e41ff766067954f6b1ff975dae219234
Jesus Emmanuel Baca Hernández <jesus.e.b.h.one@gmail.com>
2023-08-10 13:26:32 -0600

    Intentando leer correo desde controllerIndex

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -7,11 +7,12 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Session\Session;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Security\Core\Security;
-use App\Entity\User;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Symfony\Bridge\Doctrine\Security\User\UserLoaderInterface;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Entity\User;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 
 class IndexController extends AbstractController
@@ -20,10 +21,20 @@ class IndexController extends AbstractController
     public function index(Request $resquest): Response
     { 
         $user = new User();
+        dd($this->adminDashboard());
         return $this->render('index/index.html.twig', [
             'controller_name' => 'IndexController',
-            'user_name' => 'Flojo'
+            'user_name' => $role,
         ]);
+    }
+
+    // ...
+
+    public function adminDashboard(): Response
+    {
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED');
+
+        
     }
 
 }
